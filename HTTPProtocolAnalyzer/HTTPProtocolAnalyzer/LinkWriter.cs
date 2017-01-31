e33d62a532e8776c1b19bb04d7ce00ce989c8e40
VanHakobyan <van19962013@mail.ru>
2017-01-31 18:00:39 +0400

    step1

---
+++ HTTPProtocolAnalyzer/HTTPProtocolAnalyzer/LinkWriter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPProtocolAnalyzer
+{
+    public class LinkWriter : IDisposable
+    {
+        private StreamWriter sw;
+        private WebClient wc = new WebClient();
+        public LinkWriter(string path)
+        {
+            sw = new StreamWriter(path);
+        }
+        public void linkSaver(string link)
+        {
+            if (link == null)
+            {
+                throw new ArgumentNullException(nameof(link));
+            }
+
+            Uri uri = new Uri(link);
+
+            var html = wc.DownloadString(uri);
+
+            sw.Write(html);
+        }
+
+
+
+
+
+
+        public void Dispose()
+        {
+            Dispose();
+        }
+    }
+}
